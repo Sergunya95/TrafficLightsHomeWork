065ec6ef2b0a981f3a773c73eeba524a6d33c580
sergunya95 <sergunya95@gmail.com>
2020-10-14 18:43:09 +0700

    View

--- TrafficLightsHomeWork/ViewController.swift
+++ TrafficLightsHomeWork/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startNextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.2
+        yellowLightView.alpha = 0.2
+        greenLightView.alpha = 0.2
+        
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        
+        startNextButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startNextButtotPressed() {
+        if redLightView.alpha == 0.2 {
+            redLightView.alpha = 1
+            startNextButton.setTitle("Next", for: .normal)
+        } else {
+            
+        }
+    }
+    
 }
 
